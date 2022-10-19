68b233561699c198290b616ffdbd397048590ed2
Kurisuchaaaan <xtianvillamor12@gmail.com>
2022-10-20 05:48:48 +0800

    Update
    10/20/2022

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    private float bounce = 22f;
+    private float bounce = 25f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+        }
+        if (collision.gameObject.CompareTag("Player2"))
         {
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
         }
