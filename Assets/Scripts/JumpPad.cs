70b388be3d41de54e1b19cc561695d769d5791eb
sordyx <rodolfo.baldo@neu.edu.ph>
2022-11-30 00:24:09 +0800

    Revert "Some new updates"
    This reverts commit 1033c5475472f47a7998e2c764d585a1eaf39dc7, reversing
    changes made to 1f2acbd1af9e71a98bb692818c87fdac2bbb39bb.

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    [SerializeField] private float bounce = 30f;
+    private float bounce = 30f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
